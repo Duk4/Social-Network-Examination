9796d7d76348ed8d90e29ae4666ec44e00e46f65
Dule <crashendodt@gmail.com>
2019-12-19 16:38:06 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import users from './data.json';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App" >
+        Fuck code tests!
+      </div>
+    )
+  }
+}
+
+export default App;
