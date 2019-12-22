510e56538f808a4a6938de12cc44a19ccae64497
Dule <crashendodt@gmail.com>
2019-12-22 12:04:31 +0100

    Made design responsive, also fixed some algorithms

--- src/components/functions/getFriendOfAFriend.js
+++ src/components/functions/getFriendOfAFriend.js
@@ -2,19 +2,17 @@ import getFriends from './getFriends';
 
 const getFriendOfAFriend = (id) => {
     const friends = getFriends(id);
-    const ffList = [];
+    let ffList = [];
 
     for (let i = 0; i < friends.length; i++) {
         let f_friends = getFriends(friends[i].id);
-        for (let j = 0; j < f_friends.length; j++) {
-            ffList.push(f_friends[j]);
-        }
+        ffList = [...ffList, ...f_friends];
     }
 
-    let unique = Array.from(new Set(ffList.map(a => a.id)))
+    let unique = Array.from(new Set(ffList.map(el => el.id)))
         .map(id => {
-            return ffList.find(a => a.id === id)
-        })
+            return ffList.find(el => el.id === id)
+        });
 
     return unique.filter((el) => {
         return el.id != id;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/style.scss';
+import './styles/breakpoints.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
